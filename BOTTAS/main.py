4a7c925b626eeed323fde4b243c0077acc282430
DaviDemarqui <79232502+DaviDemarqui@users.noreply.github.com>
2021-07-02 19:48:27 -0300

    Add files via upload

---
+++ BOTTAS/main.py
@@ -0,0 +1,39 @@
+import random
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix = "!", intents=intents)
+
+@client.event
+async def on_ready():
+    print("The bot is on ready for use!!!")
+    print("..............................")
+
+#Commands
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello i am BOTTAS")
+
+
+@client.command()
+async def covid(ctx):
+        await ctx.send("I hate covid!")
+
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(859557683144425506)
+    await channel.send('Helloo!')
+
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(859557683144425506)
+    await channel.send('GoodBye')
+
+
+
+client.run('')#TokenHere
