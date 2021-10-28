4192c259789f32c8c4c551a7b3c4bc2a71db7a42
DaviDemarqui <79232502+DaviDemarqui@users.noreply.github.com>
2021-10-28 17:34:45 -0300

    Update main.py

--- BOTTAS/main.py
+++ BOTTAS/main.py
@@ -2,7 +2,7 @@ import random
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default();
+intents = discord.Intents.default()
 intents.members = True
 
 client = commands.Bot(command_prefix = "!", intents=intents)
