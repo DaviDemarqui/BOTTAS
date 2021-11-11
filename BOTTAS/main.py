223d24d25eb210c24c8ecc2d07bdf0740d73df48
DaviDemarqui <79232502+DaviDemarqui@users.noreply.github.com>
2021-11-11 15:53:02 -0300

    Update main.py

--- BOTTAS/main.py
+++ BOTTAS/main.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix = "!", intents=intents)
 async def on_ready():
     print("The bot is on ready for use!")
 
-#Commands
+#Commands - say hello
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello i am BOTTAS")
@@ -21,13 +21,13 @@ async def hello(ctx):
 async def covid(ctx):
         await ctx.send("I hate covid!")
 
-
+#say hello when a member join
 @client.event
 async def on_member_join(member):
     channel = client.get_channel()
     await channel.send('Helloo!')
 
-
+#say goodbye when a member quit
 @client.event
 async def on_member_remove(member):
     channel = client.get_channel()
@@ -35,4 +35,4 @@ async def on_member_remove(member):
 
 
 
-client.run('')#Put your token here!
+client.run('')#paste your discord token here
